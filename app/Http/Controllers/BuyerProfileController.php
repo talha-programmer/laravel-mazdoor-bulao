0e6b715745f33442b7630505824c78023ce9329c
Muhammad Talha <talhashabbir9@gmail.com>
2021-10-25 23:12:02 +0500

    simple index and store functions for buyer and worker profiles

--- app/Http/Controllers/BuyerProfileController.php
+++ app/Http/Controllers/BuyerProfileController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class BuyerProfileController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        return $this->middleware(['auth']);
+    }
+
+    public function index()
+    {
+        $profile = auth()->user()->buyerProfile;
+
+        return response(['buyer_profile' => $profile], 200);
+    }
+
+
+    public function store(Request $request)
+    {
+        $user = auth()->user();
+        $user->workerProfile()->create([
+
+        ]);
+
+        return response(['status' => 'Profile created successfully!']);
+    }
+
+
 }
--- app/Http/Controllers/WorkerProfileController.php
+++ app/Http/Controllers/WorkerProfileController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WorkerProfileController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $profile = auth()->user()->workerProfile;
+        return response(['worker_profile' => $profile], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'description' => 'required',
+            'is_edit' => 'boolean'
+        ]);
+
+        $user = auth()->user();
+        $user->workerProfile()->create([
+            'description' => $request->description,
+        ]);
+
+        return response(['status' => 'Profile created successfully!'], 200);
+    }
+
+
 }
