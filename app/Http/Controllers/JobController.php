487905166d14efe0fb17b7599934e1df67f93987
Muhammad Talha <talhashabbir9@gmail.com>
2021-11-10 14:40:47 +0500

    get and save images of the jobs. Also created their relationship with job

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Enums\JobStatus;
+use App\Models\Image;
 use App\Models\Job;
 
 use App\Models\JobCategory;
 use App\Models\WorkerProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Psy\Util\Str;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image as ImageLibrary;
 
 
 class JobController extends Controller
@@ -52,8 +54,16 @@ class JobController extends Controller
             'deadline' => 'required|numeric',
             'location' => 'required|string|max:255',
             'categories' => 'required|string',
-            'job_id' => 'numeric|min:1'         // in case of edit
+            'job_id' => 'numeric|min:1',         // in case of edit
+            'no_of_images' => 'numeric|min:0'
         ]);
+
+        // Single array of all images was not working
+        // That's why images are passed as:
+        // "no_of_images"
+        // "image:0"
+        // "image:1" ...
+
 
         $title = $request->title;
         $details = $request->details;
@@ -83,8 +93,39 @@ class JobController extends Controller
             'status' => JobStatus::Hiring
         ]);
 
-        $job->save();
-        $job->categories()->attach($categories);
+        if($job->save()){
+            $job->categories()->attach($categories);
+
+            $noOfImages = $request->no_of_images;
+
+            if($noOfImages > 0){
+                for ($i=0; $i<$noOfImages; $i++){
+                    // Upload images and create their relation
+                    $imageObject = new Image();
+                    $loggedInUsername = auth()->user()->username;
+
+                    $image = $request->file("image:" . $i);
+                    $imageName = time() . '-' . Str::kebab($image->getClientOriginalName());
+                    $imageUrl = "images/jobs/{$loggedInUsername}";
+
+                    $thumbnail = ImageLibrary::make($image)->resize('250', null, function ($constraint){
+                        $constraint->aspectRatio();
+                    });
+                    $thumbnailName = "thumbnail-" . $imageName;
+
+                    $image->move(public_path($imageUrl), $imageName);
+                    $thumbnail->save(public_path($imageUrl. "/$thumbnailName"));
+
+                    $imageObject->image_url = $imageUrl . "/$imageName";
+                    $imageObject->image_thumbnail_url = $imageUrl . "/$thumbnailName";
+
+                    $imageObject->imageable()->associate($job);
+                    $imageObject->save();
+                }
+            }
+
+
+        }
 
         return response(['info' => 'job has been created successfully!']);
     }
@@ -105,7 +146,7 @@ class JobController extends Controller
     public function singleJob(Job $job)
     {
         $response = [
-          'job' => $job->load(['postedBy', 'categories', 'bids.offeredBy', 'orders']),
+          'job' => $job->load(['postedBy', 'categories', 'bids.offeredBy', 'orders', 'images']),
         ];
 
         return response($response, 200);
